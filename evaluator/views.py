e5cbe1ea4b08bd89dea04a3309de5ed8326db73b
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-07-18 10:04:54 +1200

    Update views.py

--- evaluator/views.py
+++ evaluator/views.py
@@ -56,6 +56,7 @@ load_dotenv()
 
 #CHATGPT_API_KEY = os.getenv('CHATGPT_API_KEY')
 CHATGPT_API_KEY = os.environ.get('CHATGPT_API_KEY')
+print(CHATGPT_API_KEY)
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 @csrf_exempt
