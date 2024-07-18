a1694fa3d11e7979a99c4e6f0f987ab6f669b7fe
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-07-18 23:30:59 +1200

    set .env in views.py

--- evaluator/views.py
+++ evaluator/views.py
@@ -1,48 +1,4 @@
-# from django.http import JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
-# import requests
-# import json
-# from dotenv import load_dotenv
-# import os
-#
-# load_dotenv()
-#
-# CHATGPT_API_KEY = os.getenv('CHATGPT_API_KEY')
-#
-# SECRET_KEY = os.getenv('SECRET_KEY')
-#
-#
-# @csrf_exempt
-# def chatgpt_view(request):
-#     if request.method == "POST":
-#         data = json.loads(request.body)
-#         user_input = data.get("content", "")
-#
-#         api_url = "https://api.openai.com/v1/chat/completions"
-#         headers = {
-#             "Authorization": "Bearer " + CHATGPT_API_KEY,
-#             "Content-Type": "application/json",
-#         }
-#         payload = {
-#             "model": "gpt-3.5-turbo-1106",
-#             "messages": [
-#                 {"role": "system", "content": "You are an IELTS examiner.  "
-#                                               "Evaluate and give scores from 0 to 9 on each of the following aspects:"
-#                                               "Task Achievement and Response, Coherence and Cohesion, Lexical Resources, Grammatical Range and Accuracy. "
-#                                               "give a detailed feedback and example to improve  "
-#                                               "in addition, at last part give a over all score."},
-#                 {"role": "user", "content": user_input}
-#             ]
-#         }
-#
-#         response = requests.post(api_url, headers=headers, json=payload)
-#         response_data = response.json()
-#         print("Full API Response:", response_data)  # Debugging line
-#
-#         assistant_response = response_data['choices'][0]['message']['content']
-#
-#         return JsonResponse({"response": assistant_response})
-#     return JsonResponse({"error": "Invalid request"}, status=400)
+
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
